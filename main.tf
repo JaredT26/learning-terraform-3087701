66aa87870d63b265522a4a94578cf3135ffb5274
JaredT26 <166476948+JaredT26@users.noreply.github.com>
2024-05-13 16:30:30 +1000

    alter block definition

--- main.tf
+++ main.tf
@@ -39,8 +39,8 @@ module "autoscaling" {
   version = "7.4.1"
   
   name     = "blog"
-  min size = 1
-  max size = 2
+  min_size = 1
+  max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns

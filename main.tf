a3d17eee3b66de612772b5fdb7ba5cf9cce4fc52
JaredT26 <166476948+JaredT26@users.noreply.github.com>
2024-05-13 16:43:41 +1000

    updated

--- main.tf
+++ main.tf
@@ -14,10 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true 
-}
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -34,7 +30,7 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "7.4.1"
   
@@ -54,7 +50,7 @@ module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "blog_alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
 

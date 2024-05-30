d869d9b38864d9162feddd83b81018e275565caf
JaredT26 <166476948+JaredT26@users.noreply.github.com>
2024-05-30 13:46:26 +1000

    update to match course files

--- main.tf
+++ main.tf
@@ -11,8 +11,9 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = [var.ami_filter.owner]
+  owners = [var.ami_filter.owner] # Bitnami
 }
+
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -20,8 +21,9 @@ module "blog_vpc" {
   name = var.environment.name
   cidr = "${var.environment.network_prefix}.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
+
 
   tags = {
     Terraform = "true"
@@ -29,27 +31,27 @@ module "blog_vpc" {
   }
 }
 
+
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "7.4.1"
-  
-  name     = "${var.environment.name}-blog"
-  min_size = var.asg_min.default
-  max_size = var.asg_max.default
+  version = "6.5.2"
 
+  name = "blog"
+
+  min_size            = var.asg_min
+  max_size            = var.asg_max
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-  
-  image_id            = data.aws_ami.app_ami.id
   instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "${var.environment.name}-blog-alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
@@ -59,7 +61,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "${var.environment.name}-"
+      name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -75,18 +77,18 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = var.environment.name
+    Environment = "dev"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
+  version = "4.13.0"
 
-  name    = "${var.environment.name}-blog"
   vpc_id  = module.blog_vpc.vpc_id
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules       = ["all-all"]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+  default     = "t3.nano"
 }
 
 variable "ami_filter" {
   description = "Name filter and owner for AMI"
 
-  type = object ({
+  type    = object ({
     name  = string
     owner = string
-  }) 
+  })
 
   default = {
     name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
@@ -18,24 +18,24 @@ variable "ami_filter" {
 }
 
 variable "environment" {
-  description = "Development Environment"
+  description = "Deployment environment"
 
-  type = object ({
+  type        = object ({
     name           = string
     network_prefix = string
   })
   default = {
-  name           = "dev"
-  network_prefix = "10.0"
+    name           = "dev"
+    network_prefix = "10.0"
   }
 }
 
 variable "asg_min" {
-  description = "Minimum number of instances in the ASG"
+  description = "Minimum instance count for the ASG"
   default     = 1
 }
 
 variable "asg_max" {
-  description = "Maximum number of instances in the ASG"
+  description = "Maximum instance count for the ASG"
   default     = 2
 }
